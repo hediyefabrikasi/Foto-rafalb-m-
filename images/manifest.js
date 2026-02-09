eabc8fd30e88b8e6e12e46239eea2fd28a335ff5
Hediye Fabrikası <hediyefabrikasi46@gmail.com>
2026-02-10 02:41:41 +0300

    Update manifest.js

--- images/manifest.js
+++ images/manifest.js
@@ -1,7 +1,8 @@
 window.ALBUM_DATA = [
   { src: "images/1.jpg", text: "İlk anımız 💖" },
-  { src: "images/2.jpg", text: "Birlikte her şey daha güzel ✨" },
-  { src: "images/3.jpg", text: "Gülüşün dünyam 🌸" },
-  { src: "images/4.jpg", text: "Sonsuza kadar 🥹" },
-  { src: "images/5.jpg", text: "En güzel hikayemiz 💕" }
+  { src: "images/2.jpg", text: "Gülüşün kalbimi eritiyor 😍" },
+  { src: "images/3.jpg", text: "Her karede sen varsın 💕" },
+  { src: "images/4.jpg", text: "Sonsuza kadar biz 💍" },
+  { src: "images/5.jpg", text: "Kalbim seninle 💘" },
+  { src: "images/6.jpg", text: "Birlikte her şey güzel 🌸" }
 ];
