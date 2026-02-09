195c8ac10752a4e9a38be1e7658728d212f83370
Hediye Fabrikası <hediyefabrikasi46@gmail.com>
2026-02-10 02:51:29 +0300

    Update manifest.js

--- images/manifest.js
+++ images/manifest.js
@@ -4,5 +4,14 @@ window.ALBUM_DATA = [
   { src: "images/3.jpg", text: "Her karede sen varsın 💕" },
   { src: "images/4.jpg", text: "Sonsuza kadar biz 💍" },
   { src: "images/5.jpg", text: "Kalbim seninle 💘" },
-  { src: "images/6.jpg", text: "Birlikte her şey güzel 🌸" }
+  { src: "images/6.jpg", text: "Kalbim seninle 💘" },
+  { src: "images/7.jpg", text: "Kalbim seninle 💘" },
+  { src: "images/8.jpg", text: "Kalbim seninle 💘" },
+  { src: "images/9.jpg", text: "Kalbim seninle 💘" },
+  { src: "images/10.jpg", text: "Kalbim seninle 💘" },
+  { src: "images/11.jpg", text: "Kalbim seninle 💘" },
+  { src: "images/12.jpg", text: "Kalbim seninle 💘" },
+  { src: "images/13.jpg", text: "Kalbim seninle 💘" },
+  { src: "images/14.jpg", text: "Kalbim seninle 💘" },
+  { src: "images/15.jpg", text: "Birlikte her şey güzel 🌸" }
 ];
