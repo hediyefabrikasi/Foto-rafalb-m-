d8dd81e09b0e1902ee706c822744d9254ff074c0
Hediye Fabrikası <hediyefabrikasi46@gmail.com>
2026-02-10 02:30:52 +0300

    Create manifest.js

---
+++ images/manifest.js
@@ -0,0 +1,7 @@
+window.ALBUM_DATA = [
+  { src: "images/1.jpg", text: "İlk anımız 💖" },
+  { src: "images/2.jpg", text: "Birlikte her şey daha güzel ✨" },
+  { src: "images/3.jpg", text: "Gülüşün dünyam 🌸" },
+  { src: "images/4.jpg", text: "Sonsuza kadar 🥹" },
+  { src: "images/5.jpg", text: "En güzel hikayemiz 💕" }
+];
